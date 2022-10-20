256ceb9e4381428d9ab03594d3b01089317cbf98
Janob Dasturchi <76569872+GamalSamadov@users.noreply.github.com>
2022-10-20 12:33:40 +0300

    Add Template and views

--- store/urls.py
+++ store/urls.py
@@ -1,3 +1,14 @@
-urlpatterns =[
-    
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='store.home'),
+    path('product/<int:pid>', views.product, name='store.product'),
+    path('category/<int:cid>', views.category, name='store.category'),
+    path('category', views.category, name='store.category'),
+    path('cart', views.cart, name='store.cart'),
+    path('checkout', views.checkout, name='store.checkout'),
+    path('checkout/complate', views.checkout_complate,
+         name='store.checkout_complate'),
 ]
